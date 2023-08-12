febbafb894ba7edb647ba2157763160e5819bd06
kanishk <kanishkshakya1@gmail.com>
2023-08-12 14:27:10 +0530

    Delete Functionally Done--

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,59 @@
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material'
-import './Todo.css'
-import React from 'react'
-function Todo(props) {
+import React, { useState } from 'react';
+import { ListItem, ListItemText, Checkbox, IconButton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Modal from './components/Modal'; // Make sure to import the correct path for your Modal component
+import { deleteDoc, collection, getDocs } from 'firebase/firestore';
+import db from './Firebase';
+
+function TodoItem(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      // Delete the todo from Firestore
+      const querySnapshot = await getDocs(collection(db, 'todos'));
+      querySnapshot.forEach(async (doc) => {
+        if (doc.data().todo === props.text) {
+          await deleteDoc(doc.ref);
+          console.log('Document deleted: ', props.text);
+        }
+      });
+    } catch (error) {
+      console.error('Error deleting document: ', error);
+    }
+  };
+
+  // Other functions (handleDelete, etc.)
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Directly open the modal when the "Edit" icon is clicked
+  const handleEditClick = () => {
+    openModal();
+  };
+
   return (
-    <List className='list'>
-        <ListItem>
-            <ListItemText primary={props.text} secondary="Dummy Deadline ⏰"/>
-        </ListItem>
-    </List>
-  )
+    <div className="">
+      <ListItem className="list">
+        <Checkbox checked={props.completed} />
+        <ListItemText primary={props.text} secondary="Dummy Deadline 🕗" />
+        <IconButton edge="end" aria-label="edit" onClick={handleEditClick}>
+          <EditIcon />
+        </IconButton>
+        <IconButton edge="end" aria-label="delete" onClick={handleDelete}>
+          <DeleteIcon />
+        </IconButton>
+      </ListItem>
+      <Modal closeModal={closeModal} open={isModalOpen} />
+    </div>
+  );
 }
 
-export default Todo
+export default TodoItem;
